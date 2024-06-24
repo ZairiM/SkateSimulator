80d22705901f2ffc176e26013f67fef72dfa3674
camilo.pineda <camilo.pineda@unity3d.com>
2024-06-24 15:23:09 -0500

    Add Unreal Project Files

---
+++ Source/SkateSimulator/SkateSimulatorGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkateSimulatorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASkateSimulatorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASkateSimulatorGameMode();
+};
+
+
+
